a7dd2033353c468fc7fb816065c488efb60abe13
KhanyiMM <moyo.khanyi@gmail.com>
2020-08-19 11:55:34 +0200

    latest update

--- Task 8.js
+++ Task 8.js
@@ -2,12 +2,8 @@ function timeConvert(num) {
    var hours = Math.floor(num / 60);  
    var minutes = num % 60;
   if (hours >= 1 && minutes >= 1) {
-  return hours + " hours" + ", " + minutes +  " minutes";      
-} else {
-  return hours + " hour" + ", " + minutes + " minute";
+  return hours + " hour" + ", " + minutes +  " minutes";
 }
 }
 
 console.log(timeConvert(71));
-console.log(timeConvert(241));
-console.log(timeConvert(133));
